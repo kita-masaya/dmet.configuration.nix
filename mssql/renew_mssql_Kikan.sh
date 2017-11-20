cffe89e4a2cfe3d54c73735c2b36069777abf56a
tonosaman <tonosaman@gmail.com>
2017-11-20 09:28:47 +0900

    to be able to update mssql datasource even if connections remain

--- mssql/renew_mssql_Kikan.sh
+++ mssql/renew_mssql_Kikan.sh
@@ -2,6 +2,11 @@
 BAKFILE=Kikan_`date +%Y%m%d --date=yesterday`.bak
 smbclient '\\192.168.1.219\share' metalics -U metalics -c "prompt;cd MSSQL_Backup;mget ${BAKFILE}.zip"
 unzip ${BAKFILE}.zip && rm -f ${BAKFILE}.zip
-docker exec -it kikan_snap /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MetaMeta123 -Q "RESTORE DATABASE [Kikan] FROM DISK = N'/src/host/${BAKFILE}' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5, MOVE 'master_20170203' TO '/var/opt/mssql/data/Kikan.mdf', MOVE 'master_20170203_log' TO '/var/opt/mssql/data/Kikan_log.ldf'"
+docker exec -it kikan_snap /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MetaMeta123 -Q "
+ALTER DATABASE [Kikan] SET SINGLE_USER WITH ROLLBACK AFTER 60
+RESTORE DATABASE [Kikan] FROM DISK = N'/src/host/${BAKFILE}' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5, MOVE 'master_20170203' TO '/var/opt/mssql/data/Kikan.mdf', MOVE 'master_20170203_log' TO '/var/opt/mssql/data/Kikan_log.ldf'
+ALTER DATABASE [Kikan] SET MULTI_USER
+GO
+"
 rm -f ${BAKFILE}
 
