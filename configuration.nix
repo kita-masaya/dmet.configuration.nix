7bec1eb5c4c97ae99473e0ee31d26ea70532d417
tonosaman <tonosaman@gmail.com>
2018-04-29 15:36:06 +0900

    add gdrive BI document and reschedule for syncing

--- configuration.nix
+++ configuration.nix
@@ -82,7 +82,7 @@
 
   services.cron.enable = true;
   services.cron.systemCronJobs = [
-      "0 3  * * *   tono    cd /home/tono/mssql && ./renew_mssql_Kikan.sh 2>&1"
+      "0 0  * * *   tono    cd /home/tono/mssql && ./renew_mssql_Kikan.sh 2>&1"
       "0 20 * * *   tono    cd /home/tono/gdrive && ./backup_redash.sh 2>&1"
       "30 3  * * *   tono    cd /home/tono/gdrive && ./sync_gdrive.sh 2>&1"
     ];
--- gdrive/sync_gdrive.sh
+++ gdrive/sync_gdrive.sh
@@ -3,11 +3,14 @@ SYNCDIR=/home/tono/gdrive
 
 gdrive update --no-progress 1B_x9igDMlVvrmfuMZf-BuXjsqbSBf3AZ redash_backup.gz
 gdrive update --no-progress 1wl_cyHaXHAp1N8CoEAeXmnLURE_mAxfY backup_redash.sh
-gdrive update --no-progress 1ig9LDNv9JcMfueXwP5_Uo74r2uXzR7X8 sync_drive.sh
+gdrive update --no-progress 1ig9LDNv9JcMfueXwP5_Uo74r2uXzR7X8 sync_gdrive.sh
 
 wget -q -O "担当地区別売上日報出力.xlsx" 'http://localhost/api/queries/40/results.xlsx?api_key=GrVOMaibBKP7YiYpM4qhXa8Wu1OYltWKsAfilMfj'
 wget -q -O "受注伝票枚数月別集計.xlsx" 'http://localhost/api/queries/3/results.xlsx?api_key=GdTbo83zTbMCqXpWWEOZyzALbzjtWKGhrSfcA0wB'
-gdrive update --no-progress 1J9DsD-g-L8e3WX6hA_B7GqDJLqnuNNobMuYNkIv64eU 担当地区別売上日報出力.xlsx
-gdrive update --no-progress 19FEM6apkm5Yf_Rw2mOLIQ0Yr88lP9_rpAUPtNvY3TD4 受注伝票枚数月別集計.xlsx
+wget -q -O "売上グラフ用データ.csv" 'http://localhost/api/queries/42/results.csv?api_key=ohi1klsJsTCSogacGsSEHPWsqIUuYkvzftgYYxSV'
+gdrive update --no-progress 1J9DsD-g-L8e3WX6hA_B7GqDJLqnuNNobMuYNkIv64eU "担当地区別売上日報出力.xlsx"
+gdrive update --no-progress 19FEM6apkm5Yf_Rw2mOLIQ0Yr88lP9_rpAUPtNvY3TD4 "受注伝票枚数月別集計.xlsx"
+gdrive update --no-progress 1HaV2wncrxRDx_KRwaZL7dMVI4wSfsH8hxtZ6IAphFbs "売上グラフ用データ.csv"
+gdrive update --no-progress 1T6PFh0f0w2R4FTqeO5JsovFbDvPv8K_IzQCQ0roLIpE "売上グラフ用データ.csv"
 
 # gdrive sync upload --no-progress --keep-local --delete-extraneous . 1V8OelWmtvDRHxT2RGnOlgW58vv1MozV3
